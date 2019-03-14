6e58fcd799fd7b0670b0708e624b79f338753d10
Eugene Paitoo <eugenepaitoo@gmail.com>
2019-03-14 21:09:36 +0000

    refactor code

--- js/script.js
+++ js/script.js
@@ -1,47 +1,60 @@
 // An array of Objects with the quote and source as properties
-var quotes = [
+const quotes = [
     {
-        quote: "In the end, it’s not the years in your life that count. It’s the life in your years",
-        source: "Abraham Lincoln",
+        quote: "Why so serious?",
+        source: "The Joker",
+        year: 2008,
+        citation: "The Dark Knight"
 
     },
     {
-        quote: "Don’t gain the world and lose your soul, wisdom is better than silver or gold",
-        source: "Bob Marley",
+        quote: "Fear is the path to the dark side...fear leads to anger, anger leads to hate, hate leads to suffering",
+        source: "Master Yoda",
+        year: 1999,
+        citation: "Star Wars: The Phantom Menace"
 
     },
     {
-        quote: "Lighten up, just enjoy life, smile more, laugh more, and don’t get so worked up about things",
-        source: "Kenneth Branagh",
+        quote: "Keep your friends close, but your enemies closer",
+        source: "Al Pacino",
+        year: 1974,
+        citation: "The Godfather II"
 
     },
     {
-        quote: "Don’t cry because it’s over, smile because it happened",
-        source: "Ludwig Jacobowski",
+        quote: "I'm your father",
+        source: "Darth Vader",
+        year: 1980,
+        citation: "Star Wars: Empire Strikes Back"
 
     },
     {
-        quote: "I tell you, in this world being a little crazy helps to keep you sane",
-        source: "Seneca",
+        quote: "Hasta la vista, baby!",
+        source: "Terminator",
+        year: 1991,
+        citation: "Terminator 2: Judgement Day"
 
     }
 ];
 
-
-// getRandomQuote function selects and returns a random quote object
+//getRandomQuote function selects and returns a random quote object
 function getRandomQuote() {
-    var quoteIndex = Math.floor(Math.random() * quotes.length);  // selects a random quote
+    const quoteIndex = Math.floor(Math.random() * quotes.length);  // selects a random quote
     return quotes[quoteIndex];  // returns the random quote
 }
 
 //printQuote function
-function printQuote(message) {
-    var result = getRandomQuote(); // Calls and stores the getRandomQuote in a variable
-    var message = "<p class='quote'>" + result.quote + "</p>" +"<p class='source'>" + result.source + "</p>";
+function printQuote() {
+    const result = getRandomQuote(); // Calls and stores the getRandomQuote in a variable
+    let message = `
+        
+            <p class="quote">${result.quote}</p>
+            <p class="source">${result.source}<span class="citation">${result.citation}</span><span class="year">${result.year}</span></p>
+     
+    `;
     document.getElementById('quote-box').innerHTML = message;
     
 }
-
 
 
 // event listener to respond to "Show another quote" button clicks
